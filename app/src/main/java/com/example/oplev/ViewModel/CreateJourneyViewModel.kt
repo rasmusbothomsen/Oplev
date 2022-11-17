3e97dc042a584390c103fb39dc41525cd35c066a
Berfin20 <s215709@student.dtu.dk>
2022-11-17 22:59:06 +0100

    Opdateret udkast. Dropdown.
    Dropdown sat ind og opdateret viewModel
    Dropdown fungerer ikke, som den skal. Selve menuen bliver vist ved øverste venstre hjørne af parent. Har forsøgt at  fikse med offset parameter. Virker ikke.

--- app/src/main/java/com/example/oplev/ViewModel/CreateJourneyViewModel.kt
+++
@@ -1,9 +0,0 @@
-package com.example.oplev.ViewModel
-
-import com.example.oplev.Model.*
-import com.example.oplev.Data.*
-
-class CreateJourneyViewModel() {
-
-
-}
--- app/src/main/java/com/example/oplev/sites/Journy/CreateJourney.kt
+++ app/src/main/java/com/example/oplev/sites/Journy/CreateJourney.kt
@@ -1,9 +1,8 @@
 package com.example.oplev.sites.Journy
 
-import android.app.DatePickerDialog
-import android.graphics.Paint
-import android.widget.DatePicker
+import android.content.Context
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
@@ -12,13 +11,20 @@ import androidx.compose.material.icons.filled.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.DpOffset
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.unit.toSize
+import com.example.oplev.Model.Category
+import com.example.oplev.Model.Journey
 import com.example.oplev.R
 import com.example.oplev.ViewModel.CreateJourneyViewModel
 
@@ -54,13 +60,15 @@ fun createJourneyComp(createJourneyViewModel: CreateJourneyViewModel, modifier: 
                           }
                       inputTextfield("Destination",80,imageVector = Icons.Filled.LocationOn)
                      //Nedenstående skal være dropdown
-                      inputTextfield("Kategori", 80, imageVector = Icons.Filled.Warning)
-                      inputTextfield("Inviter Venner",80, imageVector = Icons.Filled.Warning)
+                      ExposedDropdownMenu(list = createJourneyViewModel.categories, imageVector = Icons.Filled.Warning)
+                          inputTextfield("Inviter Venner",80, imageVector = Icons.Filled.Warning)
                       //Nedenstående rows skal i en composable
                       Row(modifier = Modifier
                           .fillMaxWidth()
                           .height(80.dp)
                           .padding(10.dp)) {
+
+
                           /* Skal være en datepicker*/ inputFieldNoRow("Fra Dato",80, imageVector = Icons.Filled.Warning)
                           Spacer(modifier = Modifier.width(20.dp))
                           /* Skal være en "time picker"*/ inputFieldNoRow("Fra kl.",80, imageVector = Icons.Filled.Warning)
@@ -149,11 +157,88 @@ fun inputFieldNoRow(label: String, height: Int, imageVector: ImageVector){
     )
 }
 
+@Composable
+fun DatePickerTest(context : Context) {
+    val year : Int
+    val month : Int
+    val day : Int
+}
+
+@Composable
+fun ExposedDropdownMenu(list : List<Category>, imageVector: ImageVector){
+    var expanded by remember { mutableStateOf(false)
+    }
+    var listOfTitles = listOf<String>("")
+
+    for (category : Category in list){
+        listOfTitles = listOf<String>(category.title)
+    }
+
+    val list = listOfTitles
+    var selectedItem by remember { mutableStateOf("") }
+    var textFilledSize by remember { mutableStateOf(Size.Zero) }
+    val icon = if (!expanded)
+    {
+        Icons.Filled.KeyboardArrowUp
+    }
+    else
+    {
+        Icons.Filled.KeyboardArrowDown
+    }
+
+        Row() {
+            Icon(
+                imageVector = imageVector,
+                contentDescription = "",
+                modifier = Modifier.padding(20.dp, 15.dp, 15.dp, 20.dp)
+            )
+
+            OutlinedTextField(
+                value = selectedItem,
+                onValueChange = { selectedItem = it },
+                modifier = Modifier
+                    .width(280.dp)
+                    .padding(0.dp, 0.dp, 0.dp, 20.dp),
+                label = { Text(text = "Kategori") },
+                trailingIcon = {
+                    Icon(icon, "", Modifier.clickable { expanded = !expanded })
+                }
+            )
+            DropdownMenu(
+                expanded = expanded,
+                onDismissRequest = { expanded = false },
+                modifier = Modifier
+                    .width(with(LocalDensity.current) { textFilledSize.width.toDp() })
+            ) {
+                for (title: String in list) {
+                    DropdownMenuItem(onClick = {
+                        selectedItem = title
+                        expanded = false
+                    }) {
+                        Text(text = title)
+
+                    }
+                }
+
+            }
+        }
+}
+
 
 
 @Preview(showBackground = true)
 @Composable
 fun createJourneyPreview(){
-    createJourneyComp(createJourneyViewModel = CreateJourneyViewModel())
+    val journey1 = Journey("e","img_denmark",null,"","Danmark", null)
+    val journey2 = Journey("e","img_norway",null,"","Norge",null)
+    val journey3 = Journey("e","img_finland",null,"","Finland",null)
+    val journey4 = Journey("e","img_turkey",null,"","Tyrkiet",null)
+    val journeys = listOf(journey1, journey2, journey3, journey4)
+
+    val seneste = Category("Seneste", journeys)
+    val favoritter = Category("Favoritter", journeys)
+    val categories = listOf(seneste,favoritter)
+    val createJourneyViewModel = CreateJourneyViewModel(categories)
+    createJourneyComp(createJourneyViewModel)
     }
 
