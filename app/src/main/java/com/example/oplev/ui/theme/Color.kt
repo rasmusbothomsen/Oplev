1d90e22aef463d0dd940e7596ec740754c7aed30
bacondragonfish <s215715@dtu.dk>
2023-01-06 15:17:37 +0100

    Farver og billeder.

--- app/src/main/java/com/example/oplev/ui/theme/Color.kt
+++ app/src/main/java/com/example/oplev/ui/theme/Color.kt
@@ -6,10 +6,28 @@ import androidx.compose.ui.graphics.Color
  val OplevBlue = Color(0xFF6200EE)
  val OplevMain = Color(0xFF6200EE)
  val Oplevsec = Color(0xFF6200EE)
- val OplevOnboard1 = Color(0xFFC3D4E8)
- val OplevOnboard2 = Color(0xFFC3E6E8)
- val OplevOnboard3 = Color(0xFFE8C3C3)
- val OplevOnboard4 = Color(0xFFC3E8D4)
 
 
+//Vores bud på farver:
+val OptFinalOplevButton = Color(0xFF0D3B66)
+val OptFinalOplevBackground = Color (0xFFCEBBC9)
+val OptOplevOpt1 = Color (0xFF539987)
+val OptOplevOpt2 = Color (0xFFA93F55)
+val OptOplevOpt3 = Color (0xFFDB7C26)
+
+// Klientens farver
+val OplevFarve1= Color(0xFFf0494b)
+val OplevFarve2= Color(0xFF004070)
+val OplevFarve3= Color(0xFF3c4654)
+
+// Klientens farvekombi
+val Farvekombi031 = Color(0xFFFAF9F0)
+val Farvekombi032 = Color(0xFFE3C5A0)
+val Farvekombi033 = Color(0xFFECC5C9)
+
+// Onboarding farver:
+val OplevOnboard1 = Color(0xFFC3D4E8)
+val OplevOnboard2 = Color(0xFFC3E6E8)
+val OplevOnboard3 = Color(0xFFE8C3C3)
+val OplevOnboard4 = Color(0xFFC3E8D4)
 
