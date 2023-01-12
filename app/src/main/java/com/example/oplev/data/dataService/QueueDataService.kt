04c0308522689a48b71ba8ace1232332b4c16332
rasmusbothomsen <rasmus0408@gmail.com>
2023-01-12 15:04:54 +0100

    firebase restruckture!!1

--- app/src/main/java/com/example/oplev/data/dataService/QueueDataService.kt
+++ app/src/main/java/com/example/oplev/data/dataService/QueueDataService.kt
@@ -1,16 +1,21 @@
 package com.example.oplev.data.dataService
 
-import android.icu.util.TimeUnit
+import android.util.Log
 import com.example.oplev.Model.QueueItem
 import com.example.oplev.data.AppDatabase
-import com.example.oplev.data.roomDao.QueDao
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.ktx.Firebase
 import kotlinx.coroutines.*
+import kotlin.reflect.full.memberProperties
 
 class QueueDataService(
 
     val appDatabase:AppDatabase
 ) {
+    private val db : FirebaseFirestore = FirebaseFirestore.getInstance()
     val dao = appDatabase.QueDao()
+    private var itemsSynced = 0
 
     fun insertItemToQue(itemType:String,itemID:String){
         val item = QueueItem(0,itemType,itemID,true)
@@ -18,10 +23,10 @@ class QueueDataService(
     }
     suspend fun syncDatabases():Deferred<Unit>{
         return GlobalScope.async(Dispatchers.IO) {
-            delay(2000)
+            upDateFirebaseFromQueue()
         }
     }
-    suspend fun upDateFirebaseFromQueue(){
+    suspend fun upDateFirebaseFromQueue() {
         val items = dao.getAll()
         for (item in items){
             when(item.type)
@@ -31,21 +36,68 @@ class QueueDataService(
                 "Category" -> processCategory(item.objectId)
                 "Journey" -> processJourney(item.objectId)
             }
+
+            if (itemsSynced == items.size){
+                Log.d("Synced complete","QueItems synced")
+                appDatabase.clearAllTables()
+            }
+            else{
+                Log.e("SYNCED FAILED","NOT ALL ITEMS WHERE SYNCED. SYNCED: $itemsSynced: Total: ${items.size}")
+            }
         }
-
-
-    }
-
-    fun processIdea(objectIt:String){
-
-    }fun processFolder(objectIt:String){
-
-    }fun processCategory(objectIt:String){
-
-    }fun processJourney(objectIt:String){
-
     }
 
 
+    fun processIdea(objectId:String){
+       val items  = appDatabase.IdeaDao().getfromId(objectId)
+        for (item in items){
+            insertIntoFireBase(extractDataClassAttributes(item))
+        }
+    }fun processFolder(objectId:String){
+        val items  = appDatabase.FolderDao().getfromId(objectId)
+        for (item in items){
+            insertIntoFireBase(extractDataClassAttributes(item))
+        }
+    }fun processCategory(objectId:String){
+        val items  = appDatabase.CategoryDao().getfromId(objectId)
+        for (item in items){
+            insertIntoFireBase(extractDataClassAttributes(item))
+        }
+    }fun processJourney(objectId:String){
+        val items  = appDatabase.JourneyDao().getfromId(objectId)
+        for (item in items){
+            insertIntoFireBase(extractDataClassAttributes(item))
+        }
+    }
+    fun <T: Any> extractDataClassAttributes(data: T): Pair<String?, HashMap<String, Any>> {
+        val className = data::class.simpleName
+        val attributes = HashMap<String, Any>()
+        for (property in data::class.memberProperties) {
+            val attributeName = property.name
+            val attributeValue = property.call(data)
+            attributes[attributeName] = attributeValue as Any
+        }
+        return className to attributes
+    }
 
+    fun <Any> populateDatClass(data:Any):List<Any>{
+        val dbCollection = db.collection(data!!::class.simpleName!!).document(Firebase.auth.currentUser)
+    }
+
+     fun insertIntoFireBase(deconStructedItem: Pair<String?, HashMap<String, Any>>){
+
+        val collectionName = deconStructedItem.first
+        val add = deconStructedItem.second
+
+        db.collection(collectionName!!)
+            .document(Firebase.auth.currentUser?.uid.toString())
+            .set(add)
+            .addOnCompleteListener(){
+                    task ->
+                if(task.isSuccessful){
+                    Log.d("FirebaseInsert",  "STATUS: SUCCESS")
+                    itemsSynced ++
+                }
+            }
+    }
 }
--- app/src/main/java/com/example/oplev/data/roomDao/CategoryDao.kt
+++ app/src/main/java/com/example/oplev/data/roomDao/CategoryDao.kt
@@ -5,6 +5,7 @@ import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.example.oplev.Model.Category
+import com.example.oplev.Model.Idea
 import com.example.oplev.Model.Journey
 import com.example.oplev.Model.UserInfo
 
@@ -12,6 +13,8 @@ import com.example.oplev.Model.UserInfo
 interface CategoryDao:BaseDao<Category> {
     @Query("Select*from category where createdBy like :id ")
     fun getAll(id: String): List<Category>
+    @Query("select * from category where id == :stringid")
+    fun getfromId(stringid:String):List<Category>
 
     @Query("select * from Journey where categoryID like :id")
     fun getJourneysRelatedToCategory(id:String): List<Journey>
